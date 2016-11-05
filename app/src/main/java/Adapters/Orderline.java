e6512e1a4f91053e7eda61cd254965563a1d22de
anwar@css.edu.om <anwar@css.edu.om>
2016-11-05 14:48:35 +0400

    my latest build

--- app/src/main/java/Adapters/Orderline.java
+++ app/src/main/java/Adapters/Orderline.java
@@ -1,14 +1,23 @@
 package Adapters;
 
+import android.content.Context;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import ev.com.evshopapp.R;
+
+import static android.R.attr.data;
 
 /**
  * Created by code_crazy on 10/23/16.
@@ -16,38 +25,53 @@ import ev.com.evshopapp.R;
 
 public class Orderline extends RecyclerView.Adapter<Orderline.Orderline_holder> {
 
+    JSONArray orders;
+    public Orderline(JSONArray orders) {
+
+        this.orders=orders;
+
+
+    }
+
+
     @Override
     public Orderline_holder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+
+        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.card_orders,parent,false);
+
+
+        return new Orderline_holder(v);
     }
 
     @Override
     public void onBindViewHolder(Orderline_holder holder, int position) {
 
-//        try {
-//
-//
-//
-//
-//            if(data.get(position).getNumber("orderNo")!=null){
-//                holder.orderId.setText(data.get(position).getNumber("orderNo").toString());
-//            }else{
-//                holder.orderId.setText("07");
-//            }
-//
+        try {
+
+
+            JSONObject order= (JSONObject) orders.get(position);
+            Log.d("order",order.toString() + " "+position);
+
+            if(order.getString("o_id")!=null){
+                holder.orderId.setText(order.getString("o_id"));
+                holder.status.setText(order.get("status").equals(null)?"null":"test");
+            }else{
+                holder.orderId.setText("error");
+            }
+
 //
 //            holder.status.setText(data.get(position).getString("Status"));
 //            holder.total.setText(df.format(data.get(position).getNumber("Tottal").doubleValue()) + " OMR");
 //            holder.C_D.setText(String.valueOf(data.get(position).getCreatedAt()).substring(3, 10) + " " + String.valueOf(data.get(position).getCreatedAt()).substring(data.get(position).getCreatedAt().toString().length() - 4));
-//        }catch (Exception ignore){
-//
-//        }
+        }catch (Exception ignore){
+
+        }
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return orders.length();
     }
 
     public class Orderline_holder extends RecyclerView.ViewHolder
@@ -67,8 +91,8 @@ public class Orderline extends RecyclerView.Adapter<Orderline.Orderline_holder> 
             total= (TextView)itemView.findViewById(R.id.fullorder_total);
             prRecyclerView=(RecyclerView)itemView.findViewById(R.id.my_recycler_view_orderline);
             prRecyclerView.setHasFixedSize(true);
-          //  LinearLayoutManager mLayoutManager =  new LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,false);
-            //prRecyclerView.setLayoutManager(mLayoutManager);
+            LinearLayoutManager mLayoutManager =  new LinearLayoutManager(itemView.getContext(),LinearLayoutManager.HORIZONTAL,false);
+            prRecyclerView.setLayoutManager(mLayoutManager);
         //    more = (ImageButton)itemView.findViewById(R.id.order_imgbutton);
 
 
